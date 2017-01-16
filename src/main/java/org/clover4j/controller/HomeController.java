25fd0d71672af80db9fb4d5c6d96f9d29d88af63
18363998103@163.com <smallclover>
2017-01-16 20:23:57 +0800

    init commit

---
+++ src/main/java/org/clover4j/controller/HomeController.java
@@ -0,0 +1,26 @@
+package org.clover4j.controller;
+
+import org.clover4j.framework.annoation.Action;
+import org.clover4j.framework.annoation.Controller;
+import org.clover4j.framework.bean.Param;
+import org.clover4j.framework.bean.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 主页
+ * @author smallclover
+ * @create 2017-01-10
+ */
+@Controller
+public class HomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+
+    @Action(value = "get:/home")
+    public View home(Param param){
+        logger.debug("home");
+        return new View("home.jsp").addModel("welcome", "HelloWorld");
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: smallclover
+  Date: 2017/1/11
+  Time: 9:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+${welcome}
+<%=request.getAttribute("welcome")%>
+
+</body>
+</html>
